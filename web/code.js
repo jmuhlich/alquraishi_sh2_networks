3278f4e23184e5386cd1a70dd8e86a641e8fb3dd
Jeremy Muhlich <jmuhlich@bitflood.org>
2014-11-05 17:35:13 -0500

    made cutoff slider functional

--- web/code.js
+++ web/code.js
@@ -9,7 +9,7 @@
 
     var elements = data.elements;
 
-    var cy = cytoscape({
+    cy = cytoscape({
       textureOnViewport: true,
       pixelRatio: 1,
       motionBlur: false,
@@ -65,6 +65,19 @@
 
     cy.boxSelectionEnabled(false);
 
+    // Set probability data value on nodes to max of probability on incident
+    // edges. Then hide/show logic can hide "orphan" nodes trivially.
+    cy.nodes().each(function(i, ele) {
+      var edges = ele.neighborhood('edge');
+      var pmax = edges.max(function(ele) { return ele.data('probability'); });
+      ele.data('probability', pmax.value);
+    });
+
+    // Calling updateCutoff requires that cy is initialized, but this is not a
+    // very good place to put this code as it's not related to building the
+    // graph.
+    updateCutoff($slider.slider("value") / SLIDER_SCALING);
+
     // DEBUG
     window.cy = cy;
 
@@ -80,7 +93,23 @@
   }
 
   function updateCutoff(cutoff) {
+    var old_cutoff = $("#cutoff").val();
+    if (old_cutoff == "") {
+      old_cutoff = CUTOFF_MAX;
+    } else {
+      old_cutoff = parseFloat(old_cutoff);
+    }
+    // Use .toFixed so that e.g. 0.9 becomes "0.90".
     $("#cutoff").val(cutoff.toFixed(SLIDER_PREC_DIGITS));
+    if (cutoff < old_cutoff) {
+      // Cutoff decreased.
+      var sel = '[probability>=' + cutoff + '][probability<' + old_cutoff + ']';
+      cy.elements().filter(sel).show();
+    } else {
+      // Cutoff increased.
+      var sel = '[probability>=' + old_cutoff + '][probability<' + cutoff + ']';
+      cy.elements().filter(sel).hide();
+    }
   }
 
   $("document").ready(function() {
@@ -96,7 +125,6 @@
       max: 1.0 * SLIDER_SCALING,
       slide: slideCutoff,
     });
-    updateCutoff($slider.slider("value") / SLIDER_SCALING);
 
   });
 
